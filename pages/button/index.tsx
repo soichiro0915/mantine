b6ec9e4a0c67c4df9b4844bda1e10827cafdf00c
soichiro0915 <soichiro.ex.eru0915@gmail.com>
2022-12-12 12:26:31 +0900

    button実装

--- pages/button/index.tsx
+++ pages/button/index.tsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import Link from 'next/link'
+import { ReplyIcon } from '@heroicons/react/solid'
+import { Button, Group } from '@mantine/core'
+import { BrandGithub, ThreeDCubeSphere } from 'tabler-icons-react'
+
 import { Layout } from '../../components/Layout'
 
 const ButtonDemo = () => {
-  return <Layout title="button">button</Layout>
+  return (
+    <Layout title="button">
+      <Group position="center" direction="column">
+        <Button
+          classNames={{
+            leftIcon: 'text-pink-500 h-6 w-6',
+            rightIcon: 'text-orange-500 h-6 w-6',
+          }}
+          color="cyan"
+          radius="lg"
+          uppercase
+          leftIcon={<BrandGithub />}
+          rightIcon={<ThreeDCubeSphere />}
+        >
+          press
+        </Button>
+
+        <Button mt="md">Click</Button>
+
+        <Link href="/">
+          <ReplyIcon className="mt-4 h-6 w-6 cursor-pointer text-gray-500 hover:text-gray-600" />
+        </Link>
+      </Group>
+    </Layout>
+  )
 }
 
 export default ButtonDemo
